ee8ee5c000dbddd0546d00f9a280ca2693d7982d
RohanMenon2006 <realroplayz@gmail.com>
2023-05-14 00:14:52 +0530

    .

--- gsheet.py
+++ gsheet.py
@@ -17,13 +17,8 @@ workbook = file.open("TMS - Timetable")
 sheet = workbook.get_worksheet(0)
 
 # # Each row is a separate list
-# header = sheet.get("A1:I1")
-# body = sheet.get("A2:I6")
+header = sheet.get("A1:I1")
+body = sheet.get("A2:I6")
 
-# print(header)
-# print(body)
-
-file.copy("18Qk5bzuA7JOBD8CTgwvKYRiMl_35it5AwcFG2Bi5npo", title="timcard2", copy_permissions=True)
-
-worksheet = file.open("timcard2")
-worksheet.share("my_email@google.com", perm_type='user', role='writer')
+print(header)
+print(body)
